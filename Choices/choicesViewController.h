1637376e010bd5e641ef112279c5a209cfbf91da
eesierra <esierr1@gmail.com>
2013-06-24 17:34:18 -0600

    Choices
    Complete code

--- Choices/choicesViewController.h
+++ Choices/choicesViewController.h
@@ -8,6 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface choicesViewController : UIViewController
+@interface choicesViewController : UIViewController <UITextFieldDelegate>
+
+@property (nonatomic, strong) NSMutableArray *choices;
+
+@property (weak, nonatomic) IBOutlet UITextField *num1;
+@property (weak, nonatomic) IBOutlet UITextField *num2;
+@property (weak, nonatomic) IBOutlet UITextField *num3;
+@property (weak, nonatomic) IBOutlet UITextField *num4;
+@property (weak, nonatomic) IBOutlet UITextField *num5;
+
+@property (strong, nonatomic) NSString *stringFromNum1;
+@property (strong, nonatomic) NSString *stringFromNum2;
+@property (strong, nonatomic) NSString *stringFromNum3;
+@property (strong, nonatomic) NSString *stringFromNum4;
+@property (strong, nonatomic) NSString *stringFromNum5;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *go;
+@property (weak, nonatomic) IBOutlet UILabel *answer;
+
+- (IBAction)go:(id)sender;
+
 
 @end
--- Choices/choicesViewController.m
+++ Choices/choicesViewController.m
@@ -16,14 +16,63 @@
 
 - (void)viewDidLoad
 {
+    
+    self.num1.delegate = self;
+    self.num2.delegate = self;
+    self.num3.delegate = self;
+    self.num4.delegate = self;
+    self.num5.delegate = self;
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    return [textField resignFirstResponder];
+}
+
+- (IBAction)go:(id)sender
+{
+
+    self.stringFromNum1 = self.num1.text;
+    self.stringFromNum2 = self.num2.text;
+    self.stringFromNum3 = self.num3.text;
+    self.stringFromNum4 = self.num4.text;
+    self.stringFromNum5 = self.num5.text;
+    
+    self.choices = [[NSMutableArray alloc] initWithCapacity:5];
+    
+    if(![self.stringFromNum1 isEqualToString:@""])
+    {
+        [self.choices addObject:self.stringFromNum1];
+    }
+    if(![self.stringFromNum2 isEqualToString:@""])
+    {
+        [self.choices addObject:self.stringFromNum2];
+    }
+    if(![self.stringFromNum3 isEqualToString:@""])
+    {
+        [self.choices addObject:self.stringFromNum3];
+    }
+    if(![self.stringFromNum4 isEqualToString:@""])
+    {
+        [self.choices addObject:self.stringFromNum4];
+    }
+    if(![self.stringFromNum5 isEqualToString:@""])
+    {
+        [self.choices addObject:self.stringFromNum5];
+    }
+    
+    int index = arc4random() % [self.choices count];
+    self.answer.text = self.choices[index];
+
 }
 
 @end
